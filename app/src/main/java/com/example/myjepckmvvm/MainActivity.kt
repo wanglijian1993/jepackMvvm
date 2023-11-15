4d29a2bd434f8cbb666e7f814e3a34f72e548a93
wanglijian1993 <>
2023-11-15 18:02:49 +0800

    1.添加DrawableLayout侧滑栏 2.添加Toolbar

--- app/src/main/java/com/example/myjepckmvvm/MainActivity.kt
+++ app/src/main/java/com/example/myjepckmvvm/MainActivity.kt
@@ -1,14 +1,12 @@
 package com.example.myjepckmvvm
 
 import android.os.Bundle
-import android.view.MenuItem
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.myjepckmvvm.base.BaseKtActivity
 import com.example.myjepckmvvm.databinding.ActivityMainBinding
 import com.example.myjepckmvvm.mvvm.MainViewModel
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.navigation.NavigationBarView
 
 class MainActivity : BaseKtActivity<ActivityMainBinding, MainViewModel>() {
 
@@ -23,26 +21,24 @@ class MainActivity : BaseKtActivity<ActivityMainBinding, MainViewModel>() {
         val navView: BottomNavigationView = binding.bottomNav
         val navController = findNavController(R.id.nav_host_container)
         navView.setupWithNavController(navController)
-        navView.setOnItemSelectedListener(object : NavigationBarView.OnItemSelectedListener {
-            override fun onNavigationItemSelected(item: MenuItem): Boolean {
-                when (item.itemId) {
-                    R.id.navigation_home -> {
-                        navController.navigate(R.id.home)
-                    }
-
-                    R.id.navigation_dashboard -> {
-                        navController.navigate(R.id.dashboard)
-
-                    }
-
-                    R.id.navigation_notifications -> {
-                        navController.navigate(R.id.notification)
-
-                    }
+        navView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_home -> {
+                    navController.navigate(R.id.home)
                 }
-                return true
+
+                R.id.navigation_dashboard -> {
+                    navController.navigate(R.id.dashboard)
+
+                }
+
+                R.id.navigation_notifications -> {
+                    navController.navigate(R.id.notification)
+
+                }
             }
-        })
+            true
+        }
 
     }
 
