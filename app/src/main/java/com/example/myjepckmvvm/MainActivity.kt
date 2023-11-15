d8744c0c20041fae0e9b888b420771b54ba95e1e
wanglj <>
2023-11-15 14:44:45 +0800

    navigation导航功能使用用例

--- app/src/main/java/com/example/myjepckmvvm/MainActivity.kt
+++ app/src/main/java/com/example/myjepckmvvm/MainActivity.kt
@@ -1,20 +1,28 @@
 package com.example.myjepckmvvm
 
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.myjepckmvvm.base.BaseKtActivity
 import com.example.myjepckmvvm.databinding.ActivityMainBinding
 import com.example.myjepckmvvm.mvvm.MainViewModel
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : BaseKtActivity<ActivityMainBinding, MainViewModel>() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
     }
 
 
     override fun initView() {
+        val navView: BottomNavigationView = binding.bottomNav
+        val navController = findNavController(R.id.nav_host_container)
+        navView.setupWithNavController(navController)
+
     }
 
     override fun initData() {
--- app/src/main/java/com/example/myjepckmvvm/navigation/fragments/Page2Fragment.kt
+++ app/src/main/java/com/example/myjepckmvvm/navigation/fragments/Page2Fragment.kt
@@ -5,28 +5,41 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.myjepckmvvm.R
+import com.example.myjepckmvvm.databinding.FragmentPage2Binding
 
 class Page2Fragment : Fragment() {
 
+
+    private var _binding: FragmentPage2Binding? = null
+
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_page2, container, false)
+    ): View {
+        _binding = FragmentPage2Binding.inflate(inflater, container, false)
 
+        val root: View = binding.root
+        val userName = arguments?.getString("userName")
 
-        view.findViewById<Button>(R.id.bn2).setOnClickListener {
+        Toast.makeText(requireContext(), userName, Toast.LENGTH_SHORT).show()
+
+        root.findViewById<Button>(R.id.bn2).setOnClickListener {
             // 使用 Navigation 跳转，传递的 ID 为导航图中 Action标签声明的 ID
             findNavController().navigate(R.id.action_page2_to_page1)
         }
+        return root
 
+    }
 
-        return view
-
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 
 }
