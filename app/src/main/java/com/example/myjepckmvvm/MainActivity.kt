1eb407df44502a39a6086948a0e9d28f8df8b916
wanglijian1993 <>
2023-11-15 16:27:59 +0800

    navigation导航功能使用用例

--- app/src/main/java/com/example/myjepckmvvm/MainActivity.kt
+++ app/src/main/java/com/example/myjepckmvvm/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.example.myjepckmvvm
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.myjepckmvvm.base.BaseKtActivity
 import com.example.myjepckmvvm.databinding.ActivityMainBinding
 import com.example.myjepckmvvm.mvvm.MainViewModel
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationBarView
 
 class MainActivity : BaseKtActivity<ActivityMainBinding, MainViewModel>() {
 
@@ -17,15 +19,35 @@ class MainActivity : BaseKtActivity<ActivityMainBinding, MainViewModel>() {
 
     }
 
-
     override fun initView() {
         val navView: BottomNavigationView = binding.bottomNav
         val navController = findNavController(R.id.nav_host_container)
         navView.setupWithNavController(navController)
+        navView.setOnItemSelectedListener(object : NavigationBarView.OnItemSelectedListener {
+            override fun onNavigationItemSelected(item: MenuItem): Boolean {
+                when (item.itemId) {
+                    R.id.navigation_home -> {
+                        navController.navigate(R.id.home)
+                    }
+
+                    R.id.navigation_dashboard -> {
+                        navController.navigate(R.id.dashboard)
+
+                    }
+
+                    R.id.navigation_notifications -> {
+                        navController.navigate(R.id.notification)
+
+                    }
+                }
+                return true
+            }
+        })
 
     }
 
     override fun initData() {
+
 
     }
 
